fa3fc3144f2e64a4fa3d364417b9c30cdbc6601e
nrbharath97 <nr.bharath97@gmail.com>
2021-10-11 13:40:49 +0530

    Removing old file structure source code

--- fileserver/fileops/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Files
-
-class FileAdmin(admin.ModelAdmin):
-    readonly_fields = ('id',)
-
-admin.site.register(Files, FileAdmin)
--- fileserver/fileops/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Files(models.Model):
-    fs_file = models.FileField(unique=True)
